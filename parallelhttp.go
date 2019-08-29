feb06de3d6bc6500217ebb06ac49224b7b9d7ac4
Arnaud Georgin <arnaud.georgin@gmail.com>
2019-08-29 11:20:57 +0100

    using shared client & handling errors

--- parallelhttp.go
+++ parallelhttp.go
@@ -3,7 +3,6 @@ package parallelhttp
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -14,10 +13,14 @@ func DoParallelHttpCall(requests map[string]HttpRequest) map[string]HttpResponse
 	responses := make(map[string]HttpResponse, len(requests))
 	// create Go channel to communicate asynchronously
 	ch := make(chan HttpResponse)
+	// creating shared client between goroutines
+	client := &http.Client{}
+	// defer closing all connections after function returns
+	defer client.CloseIdleConnections()
 
-	// for each request we start a go routine to make all calls asynchronously
+	// for each request we start a goroutine to make all calls asynchronously
 	for _, request := range requests {
-		go doHttpCall(request, ch)
+		go doHttpCall(client, request, ch)
 	}
 
 	// we wait until all responses are received
@@ -28,21 +31,29 @@ func DoParallelHttpCall(requests map[string]HttpRequest) map[string]HttpResponse
 		numberResponsesReceived += 1
 	}
 
+	// closing channel
+	close(ch)
+
 	return responses
 }
 
-func doHttpCall(request HttpRequest, ch chan HttpResponse) {
-	res, err := http.Get(request.Url)
+func doHttpCall(client *http.Client, request HttpRequest, ch chan HttpResponse) {
+	req, err := http.NewRequest(request.Method, request.Url, nil)
 	if err != nil {
-		log.Fatal(err)
+		ch <- HttpResponse{request, "", 400, err}
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		ch <- HttpResponse{request, "", res.StatusCode, err}
 	}
 
 	responseBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		ch <- HttpResponse{request, "", 422, err}
 	}
 	res.Body.Close()
 
 	// sending HttpResponse to channel
-	ch <- HttpResponse{request, string(responseBody)}
+	ch <- HttpResponse{request, string(responseBody), res.StatusCode, nil}
 }
